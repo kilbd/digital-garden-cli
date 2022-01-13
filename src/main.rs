30dae2a2f358d8a6cf018600224839b47a2c1ad5
Drew Kilb <drew.kilb@duke.edu>
2022-01-13 10:11:53 -0500

    refactor: Use Clap v3 instead of StructOpt for argument parsing
    The new version of Clap subsumes StructOpt and offers the derive
    approach from this workshop in addition to the original builder
    approach. Most changes are to attribute names, but a few other
    minor changes too. Seems to work!

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
+use clap::{Parser, Subcommand};
 use color_eyre::eyre::{eyre, Result, WrapErr};
 use digital_garden::write;
 use directories::UserDirs;
 use std::path::PathBuf;
-use structopt::StructOpt;
 
 /// A CLI for the growing and curation of a digital garden
 ///
 /// Visit https://www.rustadventure.rs/garden for more!
-#[derive(Debug, StructOpt)]
-#[structopt(name = "garden")]
-struct Opt {
-    #[structopt(parse(from_os_str), short = "p", long, env)]
+#[derive(Debug, Parser)]
+#[clap(name = "garden")]
+struct Cli {
+    #[clap(parse(from_os_str), short = 'p', long, env)]
     garden_path: Option<PathBuf>,
-    #[structopt(subcommand)]
+    #[clap(subcommand)]
     cmd: Command,
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Subcommand)]
 enum Command {
     /// Write something in your garden
     ///
@@ -25,7 +25,7 @@ enum Command {
     /// your garden.
     Write {
         /// Optionally set a title for what you are going to write about
-        #[structopt(short, long)]
+        #[clap(short, long)]
         title: Option<String>,
     },
 }
@@ -37,12 +37,12 @@ fn get_default_garden_dir() -> Result<PathBuf> {
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let opt = Opt::from_args();
-    let garden_path = match opt.garden_path {
+    let cli = Cli::parse();
+    let garden_path = match cli.garden_path {
         Some(pathbuf) => Ok(pathbuf),
         None => get_default_garden_dir().wrap_err("`garden_path` was not supplied"),
     }?;
-    match opt.cmd {
+    match cli.cmd {
         Command::Write { title } => write(garden_path, title),
     }
 }
